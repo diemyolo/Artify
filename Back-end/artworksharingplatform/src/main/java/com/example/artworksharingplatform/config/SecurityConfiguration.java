9d0518f118cc0bff66d1dedb58822ee160a8a0c9
Lâm Minh Đăng <2603lamminhdang@gmail.com>
2024-02-26 13:50:04 +0700

    update swagger

--- Back-end/artworksharingplatform/src/main/java/com/example/artworksharingplatform/config/SecurityConfiguration.java
+++ Back-end/artworksharingplatform/src/main/java/com/example/artworksharingplatform/config/SecurityConfiguration.java
@@ -32,6 +32,19 @@ public class SecurityConfiguration {
                 .csrf(csrf -> csrf.disable())
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers("/api/auth/**").permitAll()
+                        .requestMatchers(
+                                "/api/auth/**",
+                                "/v2/api-docs",
+                                "/v3/api-docs",
+                                "/v3/api-docs/**",
+                                "/swagger-resources",
+                                "/swagger-resources/**",
+                                "/configuration/ui",
+                                "/configuration/security",
+                                "/swagger-ui/**",
+                                "/webjars/**",
+                                "/swagger-ui.html"
+                        ).permitAll()
                         .requestMatchers("/api/auth/audience/**").hasRole(AUDIENCE.name())
                         .requestMatchers("/api/auth/admin/**").hasRole(ADMIN.name())
                         .requestMatchers("api/auth/creator/**").hasRole(CREATOR.name())
