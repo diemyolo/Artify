23055fa3e26530ee86e2aef4d156b001a218c749
Nguyen Huynh Ngoc Linh <ngoclinhh1308@gmail.com>
2024-02-29 22:43:52 +0700

    Update Homepage

---
+++ Front-end/src/constants/index.js
@@ -0,0 +1,22 @@
+export const navLinks = [
+    {
+        link: "Home",
+        path: "home",
+    },
+    {
+        link: "Art",
+        path: "art",
+    },
+    {
+        link: "Artist",
+        path: "artist",
+    },
+    {
+        link: "About",
+        path: "about",
+    },
+    {
+        link: "About",
+        path: "about",
+    }
+]
