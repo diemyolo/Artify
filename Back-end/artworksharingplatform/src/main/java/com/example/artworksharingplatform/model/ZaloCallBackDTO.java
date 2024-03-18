93bceca8e587db23e303ed26dc9c3cfb08395896
bao <baocao494@gmail.com>
2024-03-18 10:39:56 +0700

    up

---
+++ Back-end/artworksharingplatform/src/main/java/com/example/artworksharingplatform/model/ZaloCallBackDTO.java
@@ -0,0 +1,12 @@
+package com.example.artworksharingplatform.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class ZaloCallBackDTO {
+	private String jsonString;
+}
