22535eaafbea682fe8a446de9e1a207be0874de1
DaoHuongThao <daothao148@gmail.com>
2024-03-18 21:44:51 +0700

    view transaction list

--- Back-end/artworksharingplatform/src/main/java/com/example/artworksharingplatform/service/FollowingService.java
+++ Back-end/artworksharingplatform/src/main/java/com/example/artworksharingplatform/service/FollowingService.java
@@ -1,5 +1,7 @@
 package com.example.artworksharingplatform.service;
 
+import java.util.List;
+
 import com.example.artworksharingplatform.entity.User;
 
 public interface FollowingService {
@@ -8,5 +10,6 @@ public interface FollowingService {
     public Integer NumOfFollowing(User creator) throws Exception;
 
     public Boolean IsFollow(User audience, User creator) throws Exception;
+
     public List<User> GetAllFollower(User audience, User creator) throws Exception;
 }
